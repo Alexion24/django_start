b6e545efaaba63cd31b62f5946cc49b50349400d
Alexion <alex24bryant@mail.ru>
2022-07-02 15:27:15 +0500

    add history page and db

--- hello_django/calc/urls.py
+++ hello_django/calc/urls.py
@@ -5,8 +5,9 @@ from hello_django.calc import views
 urlpatterns = [
     path('', views.IndexView.as_view()),
     path(
-        '<int:a>/<int:b>',
+        '<int:a>/<int:b>/',
         views.Calc.as_view(),
         name='calc'
-    )
+    ),
+    path('history/', views.history),
 ]
--- hello_django/calc/views.py
+++ hello_django/calc/views.py
@@ -1,6 +1,13 @@
+from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 from django.views import View
+from hello_django.calc.models import History
+
+
+def history(request):
+    sums = History.objects.order_by('-timestamp').all()[:10:-1]
+    return render(request, 'calc/history.html', context={'sum': sums})
 
 
 class Calc(TemplateView):
@@ -12,6 +19,8 @@ class Calc(TemplateView):
         context['first_number'] = a
         context['second_number'] = b
         context['sum'] = (a + b)
+        h = History(value=context['sum'])
+        h.save()
         return context
 
 
